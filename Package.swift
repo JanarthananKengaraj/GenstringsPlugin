1954c263b1ef0b6984a6d8757ac75d30ac2ddb47
JanaKiOS <Janarthanankece@gmail.com>
2022-09-27 17:17:10 +0530

    Update

--- Package.swift
+++ Package.swift
@@ -6,23 +6,11 @@ import PackageDescription
 let package = Package(
     name: "GenstringsPlugin",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "GenstringsPlugin",
-            targets: ["GenstringsPlugin"]),
+        .plugin(name: "GenstringsPlugin", targets: ["GenstringsPlugin"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "GenstringsPlugin",
-            dependencies: []),
-        .testTarget(
-            name: "GenstringsPluginTests",
-            dependencies: ["GenstringsPlugin"]),
+        .plugin(name: "GenstringsPlugin", capability: .buildTool())
     ]
 )
