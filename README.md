52351c9d4656319ee6f5fd21dfb12c278aa859f4
JanaKiOS <Janarthanankece@gmail.com>
2022-09-27 17:13:46 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GenstringsPlugin
+
+A description of this package.
---
+++ Sources/GenstringsPlugin/GenstringsPlugin.swift
@@ -0,0 +1,6 @@
+public struct GenstringsPlugin {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
